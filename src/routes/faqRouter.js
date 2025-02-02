7a80c77d14061235726091092b41c4bd1e0135a4
Utkarsh kumar <uk10234567@gmail.com>
2025-02-02 17:10:53 +0530

    refactor: reafactored code

--- src/routes/faqRouter.js
+++ src/routes/faqRouter.js
@@ -3,6 +3,7 @@ import { getFaq } from "../controllers/faq.get.controller.js";
 import { postFaq } from "../controllers/faq.post.controller.js";
 import { deleteFaq } from "../controllers/faq.del.controller.js";
 import { redisClient } from "../config/redis.config.js";
+import { editFaq } from "../controllers/faq.put.controller.js";
 
 const router= Router();
 
@@ -16,5 +17,6 @@ router.get('/faq', async(req, res) => {
 router.get('/GET/faqs', getFaq);
 router.post('/POST/faqs', postFaq);
 router.delete('/DELETE/faqs/:id', deleteFaq);
+router.put('/PUT/faqs/:id', editFaq);
  
 export default router
